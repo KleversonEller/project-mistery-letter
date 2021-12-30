ec8df4a1325fdc4d3cd15a6d964fb29200d899c5
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-30 04:13:58 -0300

    Requisito - 16

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 window.onload = function () {
-  //? Requisito 3 - 4 - 5
+  //? Requisito 3 - 4 - 5 - 16
+
+  function geraNumero(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
   let botaoCriaCarta = document.querySelector('#criar-carta');
 
   botaoCriaCarta.addEventListener('click', function () {
+    let estilo = ['newspaper', 'magazine1', 'magazine2'];
+    let tamanho = ['medium', 'big', 'reallybig'];
+    let rotacao = ['rotateleft', 'rotateright'];
+    let inclinacao = ['skewleft', 'skewright'];
+
     let textoDigitado = document.querySelector('#carta-texto');
     let textoDigitadoSeparado = textoDigitado.value.split(' ');
     let paragrafo = document.querySelector('#carta-gerada');
@@ -15,6 +26,12 @@ window.onload = function () {
       for (let palavra of textoDigitadoSeparado) {
         let cria = document.createElement('span');
         cria.innerHTML = palavra;
+        cria.classList.add(
+          estilo[geraNumero(0, 3)],
+          tamanho[geraNumero(0, 3)],
+          rotacao[geraNumero(0, 2)],
+          inclinacao[geraNumero(0, 2)]
+        );
         paragrafo.appendChild(cria);
       }
     }
